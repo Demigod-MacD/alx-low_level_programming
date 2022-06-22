dc4d4e3213803cfdd012f71425bb355aa792acfd
Macdonald N.O <macdonaldonwuma@gmail.com>
2022-06-22 02:38:26 -0700

    print comb5 hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 99; i++)
 	{
 		for (j = 0; j < 100; j++)
 		{
@@ -17,7 +17,7 @@ int main(void)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
-				putchar(" ");
+				putchar(32);
 				putchar((j / 10) + 48);
 				putchar((j / 10) + 48);
 				if (i != 98 || j != 99)
