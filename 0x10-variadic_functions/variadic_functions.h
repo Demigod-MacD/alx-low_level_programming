bb62226d1da51ae54718eb5a5f0eed8cc2f6783a
Macdonald N.O <macdonaldonwuma@gmail.com>
2022-07-15 12:27:03 -0700

    variadic functions hot fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,13 +5,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-}
-print_t;
-
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
