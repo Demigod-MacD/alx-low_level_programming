4b7e41955f138542a6c4b8103ecc7146205246a0
Macdonald N.O <macdonaldonwuma@gmail.com>
2022-07-15 11:23:29 -0700

    funtion pointers hot fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,18 +15,20 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-	}
+	};
 
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (ops[i].op)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (strcmp(s, ops[i].op) == 0)
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
 
-	return (NULL);
+	printf("Error\n");
+
+	exit(99);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 typedef __SIZE_TYPE__ size;
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
