fde07ee90ebba266b69a155d7cb028b8b5bf3c0a
Macdonald N.O <macdonaldonwuma@gmail.com>
2022-07-20 03:00:55 -0700

    print list int

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,18 +2,16 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: head of the list
- * @next: go to the next node
- * Description: h->next goes to the next node
+ * @h: pointer to first node
+ *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t node_count = 1;
-
+	/* return 0 as no of nodes when h is null*/
 	if (h == NULL)
 		return (0);
-
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
@@ -21,8 +19,10 @@ size_t print_listint(const listint_t *h)
 		/*go to the next node*/
 		h = h->next;
 
+		/*count no of nodes*/
 		node_count++;
 	}
+	/*print last node*/
 	printf("%d\n", h->n);
 
 	return (node_count);
