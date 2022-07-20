059eb09bf57837f52096eb6e46f39c60862da9e9
Macdonald N.O <macdonaldonwuma@gmail.com>
2022-07-20 04:34:18 -0700

    advanced task hotfix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,74 +1,43 @@
 #include "lists.h"
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * free_listp2 - frees a linked list
- * @head: head of a list
+ * free_listint_safe - thsi function free a list in safe mode
+ * @h: the head of list
+ * Description: this function free a string in a safe mode
+ * section header: the header of this function is lists.h)*
  *
- * Return : no return
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - frees a listint_t linked list
- * @head: double pointer to the start of the list
- *
- * Return: the number of nodes in the list
+ * Return: the size of the list
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nnodes = 0;
-	listp_t *hptr, *new, *add;
-	listint_t *curr;
+	listint_t *tmp, *actual;
+	size_t i;
+	int rest;
 
-	hptr = NULL;
-	while (*h != NULL)
+	i = 0, actual = *h;
+
+	while (actual)
 	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-
-		while (add->next != NULL)
+		rest = actual - actual->next;
+		if (rest > 0)
 		{
-			add = add->next;
-			if (*h == add->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (nnodes);
-			}
+			tmp = actual->next;
+			free(actual);
+			actual = tmp;
+			i++;
 		}
-		curr = *h;
-		*h = (*h)->next;
-		free(curr);
-		nnodes++;
+		else
+		{
+			free(actual);
+			*h = NULL;
+			i++;
+
+			break;
+		}
 	}
 	*h = NULL;
-	free_listp2(&hptr);
-	return (nnodes);
+
+	return (i);
 }
