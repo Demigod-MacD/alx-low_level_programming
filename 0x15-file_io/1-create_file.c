b40fbdf4dd2fc9d864cd817ca2a888e5097e14c6
Macdonald N.O <macdonaldonwuma@gmail.com>
2022-07-25 20:58:32 -0700

    create file task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,21 +5,6 @@
 #include <fcntl.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
  * create_file - creates a file
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
@@ -28,18 +13,22 @@ size_t _strlen(char *str)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t len = 0;
+	int fd, x, y = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
-	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
+	if (text_content)
+	{
+		while (text_content[y])
+			y++;
+		x = write(fd, text_content, y);
+		if (x != y)
+			return (-1);
+	}
 	close(fd);
-	if (len == -1)
-		return (-1);
+
 	return (1);
 }
